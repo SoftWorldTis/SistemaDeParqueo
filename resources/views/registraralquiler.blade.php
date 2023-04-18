08035d5b1e71ebca2582f290c6ae566fcd8eedd7
Kami Bloodfallen <juliocesarpayaargote@gmail.com>
2023-04-18 03:51:04 -0400

    lleno de errores

--- resources/views/registraralquiler.blade.php
+++ resources/views/registraralquiler.blade.php
@@ -8,80 +8,78 @@
     <div class="tit">
         <p> Alquilar sitio del parqueo</p>
     </div>
-    
-    <div class="botonesformu">
-        
-        <div class ="agregarParqueo">
-            <p class="nom">Parqueo</p>
-            <input class="botonAgregar" class="agregarP"type="button" value="Agregar" onclick="">
+    <form action="/lobby/Alquiler" method="Post">
+     @csrf <!-- debajo de un forma pones eso atte kiri-->
+        <div class="botonesformu">
+            
+            <div class ="agregarParqueo">
+                <p class="nom">Parqueo</p>
+                <input class="botonAgregar" class="agregarP"type="text" name="park" value="" onclick="">
+            </div>
+            
+            <div class ="agregarUsuario">
+                <p class="nom">Usuario</p>
+                <!--cambiar type="button" y value ="Agregar"-->
+                <input class="botonAgregar" class="agregarU" type="text" value="" name="nombre" onclick="">
+            </div>
         </div>
         
-        <div class ="agregarUsuario">
-            <p class="nom">Usuario</p>
-            <input class="botonAgregar" class="agregarU"type="button" value="Agregar" onclick="">
+        <div class="fila2">
+            
+            <div  class="Duracion">
+                <p class="nom">Duración</p>
+                <input type="date" class="linea" class="fecha"  name="date1" placeholder="FechaInicio">
+                <input type="date" class="linea2" class="fecha"  name="date2"placeholder="FechaFin">
+            </div>
+            <div class="Horario">
+                <p class="nom">Horario</p>
+                <!--readonly es para evitar su edicion en la vista (creo que se rellana automaticamente)-->
+                <input type="text" class="linea" name="hora"  >
+            </div>
         </div>
-    </div>
-    
-    <div class="fila2">
+        <div class="fila3">
+            <div class="seleccion">
+                <p class="nom">Sitio</p>
+                <select name="Sitio" class="linea" class="sitio">
+                    <option value="Sitio 10">Sitio 10</option>
+                </select>
+            </div>
+            
+            <div class="Precio">
+                <p class="nom">Precio</p>
+                <!--readonly es para evitar su edicion en la vista (creo que se rellana automaticamente)-->
+                <input type="text" class="linea" name="costo"  >
+            </div>
+            
+        </div>
         
-        <div  class="Duracion">
-            <p class="nom">Duración</p>
-            <input type="date" class="linea" class="fecha"  placeholder="FechaInicio">
-            <input type="date" class="linea2" class="fecha"  placeholder="FechaFin">
-        </div>
-        <div class="Horario">
-            <p class="nom">Horario</p>
-            <!--readonly es para evitar su edicion en la vista (creo que se rellana automaticamente)-->
-            <input type="text" class="linea" readonly="readonly" >
-        </div>
-    </div>
-    <div class="fila3">
-        <div class="seleccion">
-          <p class="nom">Sitio</p>
-            <select name="Sitio" class="linea" class="sitio">
-            </select>
-      </div>
-      
-      <div class="Precio">
-        <p class="nom">Precio</p>
-         <!--readonly es para evitar su edicion en la vista (creo que se rellana automaticamente)-->
-        <input type="text" class="linea" readonly="readonly" >
-      </div>
-
-    </div>
-
-    <div class="fila4">
-        <div class="rad">
-            <label class="nom2">
-                <input type="radio" class=pagar name=pago>
-                Pagar Ahora
-            </label>
-        </div>
-        <div class="rad">
-            <label class="nom2">
-                <input type="radio" class=pagar name=pago>
-                Pagar Después
-            </label>
+        <div class="fila4">
+            <div class="rad">
+                <label class="nom2">
+                    <input type="radio" class=pagar name=pago>
+                    Pagar Ahora
+                </label>
+            </div>
+            <div class="rad">
+                <label class="nom2">
+                    <input type="radio" class=pagar name=pago checked>
+                    Pagar Después
+                </label>
+            </div>
         </div>
     </div>
-</div>
     
-@endsection
-@section('botones')
-
+    @endsection
+    @section('botones')
+    
 </div>
 </div>
 <div class="AbBotones">
-
-<div class="cancelar button">
-
-<p>Cancelar</p>
-</div>
-
-
-<div class="guardar button">
-   
-<p>Guardar</p>
-    
-</div>   
-@endsection
+    <div class="cancelar button">
+        <p>Cancelar</p>
+    </div>
+    <button class="guardar button"  type="submit" >
+   <p>Guardar</p>
+   </button>   
+   </form>
+   @endsection
