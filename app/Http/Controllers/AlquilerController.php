3143e5c62438f3004534f2cdb708d502e06a065f
ayeye's servant <sami287borda@gmail.com>
2023-05-03 03:51:49 -0400

    cliente funcion

--- app/Http/Controllers/AlquilerController.php
+++ app/Http/Controllers/AlquilerController.php
@@ -14,21 +14,40 @@ class AlquilerController extends Controller
         $parqueo = estacionamiento::all();
         $clientes = cliente::all();
         $seleccionado="";
-        return view('registraralquiler', compact('parqueo', 'clientes', 'seleccionado'));
+        $seleccionadoes="";
+        $valorcl = $request->input('usuariosdatosci');
+        session(['valorcl' => $valorcl]);
+        $js ="";
+        return view('registraralquiler', compact('parqueo', 'clientes', 'seleccionado','seleccionadoes','valorcl','js'));
     }
 
-    public function show($id){
+    public function show(Request $request,$id){
         //$seleccionado =  estacionamiento::where('estacionamientoid', $id)
         //->first();
+      
+        //dd($seleccionado);
+        $parqueo = estacionamiento::all();
+        $clientes = cliente::all();
         $seleccionado = DB::table('estacionamiento')
         ->select('*')
         ->where('estacionamientoid','=',$id)
         ->get()
         ->first();
-        //dd($seleccionado);
-        $parqueo = estacionamiento::all();
-        $clientes = cliente::all();
-        return view('registraralquiler', compact('parqueo', 'clientes', 'seleccionado'));
+
+        // Escribir el archivo JS
+        $js = "window.onload= function() {
+            document.getElementById('miEmergente').style.display = 'none';
+            document.getElementById('mostrarEmergente').style.display = 'none';
+            document.getElementById('oculto').style.display='block';
+        }";
+    
+      
+
+        $valorcl = session('valorcl');
+        $seleccionadoes =$seleccionado->estacionamientozona;
+
+
+        return view('registraralquiler', compact('parqueo', 'clientes', 'seleccionado' ,'seleccionadoes','valorcl','js'));
     }
 
     public function store(Request $request){
