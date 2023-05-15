21dab3eee0d1dab382a5f77c3585577365496a76
Kami Bloodfallen <juliocesarpayaargote@gmail.com>
2023-05-15 02:01:34 -0400

    Correcion
    encriptación en campo contraseña y bug en el mismo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -41,15 +41,16 @@
                     @csrf
                     <div class="Usuario">
                         <span class="nombre">Usuario</span>
-                        <input type="text"  class="linea" name="name" >
+                        <input type="text" class="linea" name="name" >
                         @error('name')
                         <b class="error">{{$message}}</b>
                         @enderror
                     </div>
                     <div class="Contraseña">
                         <span class="contra">Contraseña</span>
-                        <input type="text" class="linea" name="password" >
-                         @error('name')
+                        <input  
+                        type="password" class="linea" name="password" >
+                         @error('password')
                         <b class="error">{{$message}}</b>
                         @enderror
                     </div>
